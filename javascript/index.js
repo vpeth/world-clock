cf34c5e32f17c5bfaa34f2c950055be597b04f37
vpeth <vblackgold21@gmail.com>
2024-10-11 16:51:32 -0600

    Added current location

--- javascript/index.js
+++ javascript/index.js
@@ -36,6 +36,9 @@ function updateTime() {
 
 function updateCity(event) {
   let cityTimeZone = event.target.value;
+  if (cityTimeZone === "current") {
+    cityTimeZone = moment.tz.guess();
+  }
   let cityName = cityTimeZone.split("/")[1];
   let cityTime = moment().tz(cityTimeZone);
   let citiesElement = document.querySelector("#cities");
