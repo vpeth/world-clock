f0b701f08e3e2be554c446e57eff19c266d6105e
vpeth <vblackgold21@gmail.com>
2024-10-11 15:11:07 -0600

    Added the JavaScript and Moment.js was integrated on index file.

--- javascript/index.js
+++ javascript/index.js
@@ -1,1 +1,37 @@
-alert("Hey!");
+function updateTime() {
+  ///london
+  let londonElement = document.querySelector("#london");
+  let londonDateElement = londonElement.querySelector(".date");
+  let londonTimeElement = londonElement.querySelector(".time");
+  let londonTime = moment().tz("Europe/London");
+
+  londonDateElement.innerHTML = londonTime.format("MMMM Do YYYY");
+  londonTimeElement.innerHTML = londonTime.format(
+    "h:mm:ss [<small>]A[</small>]"
+  );
+
+  ///newYork
+  let newYorkElement = document.querySelector("#newYork");
+  let newYorkDateElement = newYorkElement.querySelector(".date");
+  let newYorkTimeElement = newYorkElement.querySelector(".time");
+  let newYorkTime = moment().tz("America/New_York");
+
+  newYorkDateElement.innerHTML = newYorkTime.format("MMMM Do YYYY");
+  newYorkTimeElement.innerHTML = newYorkTime.format(
+    "h:mm:ss [<small>]A[</small>]"
+  );
+
+  ///berlin
+  let berlinElement = document.querySelector("#berlin");
+  let berlinDateElement = berlinElement.querySelector(".date");
+  let berlinTimeElement = berlinElement.querySelector(".time");
+  let berlinTime = moment().tz("Europe/Berlin");
+
+  berlinDateElement.innerHTML = berlinTime.format("MMMM Do YYYY");
+  berlinTimeElement.innerHTML = berlinTime.format(
+    "h:mm:ss [<small>]A[</small>]"
+  );
+}
+
+updateTime();
+setInterval(updateTime, 1000);
